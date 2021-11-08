0d1673cedad1fb425412ef33ac73c191cc420f02
Yumapon <keropon1274@gmail.com>
2021-11-09 00:36:31 +0900

    ok

--- getinputdata/getinputdata/__init__.py
+++ getinputdata/getinputdata/__init__.py
@@ -4,8 +4,8 @@ import azure.functions as func
 import requests
 import datetime
 
-from cosmos import DatabaseConnection
-from cosmos import getItem, getReplacedItem
+from getinputdata.cosmosdb import DatabaseConnection
+from getinputdata.cosmosdb import getItem, getReplacedItem
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -28,26 +28,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     #reqから照度、温度を取得
     illuminance = req.params.get('illuminance')
     temp = req.params.get('temp')
+    print(illuminance)
+    print(temp)
 
     #SoujiさんAPIで湿度を取得
+    print('APIを呼び出します')
     url = "https://hack2021goudou.azurewebsites.net/api/gethumid?code=gjHcmUhiNajYsy8f18vkEsV2JsoGo1LUWtEHz1A/yykhrob66kqA3w=="
     apireq = requests.get(url)
     #TODO: Soujiさんにcode:関数キーが何か確認
     pyaload = {"code":"xxxx"}
-    humid = req.params.get('humid')
+    data = apireq.json()
+    humid = data['humid']
+
+    print('humid:' + humid)
 
     #TODO: なんとかAzure Functionのoutとバインディングできそうだが、面倒すぎるのでやめる。
     #Cosmosdbの準備
     dbConnection = DatabaseConnection()
 
-    #Cosmosdbの初期化
-    print(dbConnection.initialize_database())
-    print(dbConnection.initialize_container())
+    #Cosmosdbの初期化（不要）
+    #print(dbConnection.initialize_database())
+    #print(dbConnection.initialize_container())
 
     #主キーとするタイムスタンプを取得し、Cosmosdbへ格納する
     DIFF_JST_FROM_UTC = 9
-    now = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+    now = (datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)).strftime('%Y%m%d%H%M')
+    print('now:' + now)
     dbConnection.create_item(getItem(now, illuminance, temp, humid))
 
-    return func.HttpResponse("Success!", status_code=200)
-
+    return func.HttpResponse(
+             "Put OK!!!",
+             status_code=200
+        )
